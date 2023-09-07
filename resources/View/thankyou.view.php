d025bc1ea07caddf3f9264fe7131834efed35ffe
TSMMONKEY <thabanimat24@gmail.com>
2023-09-07 11:47:30 +0200

    fixed all routed and added a page for each route

---
+++ resources/View/thankyou.view.php
@@ -0,0 +1,42 @@
+<?php require '../public/head.view.php'; ?>
+
+<main>
+      <section class="donate-section" style="padding: 0">
+        <div class="container-fluid" style="padding: 0">
+          <div class="row" style="width: 100%; margin: 0; padding: 0">
+            <div class="col-lg-12 col-12 mx-auto" style="margin: 0; padding: 0">
+              <section
+                class="cta-section section-padding section-bg"
+                style="height: 47vh"
+              >
+                <div class="container">
+                  <div class="row justify-content-center align-items-center">
+                    <div class="col-lg-5 col-12 ms-auto">
+                      <h2 class="mb-0">
+                        Thank you for signing up with Gondo Skills Institute
+                        Security Services!
+                      </h2>
+                    </div>
+
+                    <div class="col-lg-5 col-12">
+                      <p>
+                        We are thrilled to have you as a member of our
+                        community. Your interest in our services demonstrates
+                        your commitment to enhancing your security skills and
+                        knowledge.
+                      </p>
+
+                      <a href="/" class="custom-btn btn smoothscroll"
+                        >Back to Home
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+      </section>
+</main>
+
+    <?php require '../public/footer.view.php'; ?>
